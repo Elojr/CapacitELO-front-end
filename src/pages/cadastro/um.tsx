a9b6e425627756830d410a00dab5119c163ba26e
Lara Portilho <lara.portilho.m@gmail.com>
2021-06-28 17:43:02 -0300

    Pagina um do cadastro

---
+++ src/pages/cadastro/um.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Cadastro1: React.FC = () => {
+    return (
+        <div className="cad1">
+            <main>
+                <div className="box-cad1">
+                    <h1>Bem vindo ao</h1>
+                    <input type="text" placeholder="Nome Completo" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Senha*" />
+                    <input type="password" placeholder="Confirmar Senha" />
+                    <button>Entrar</button>
+                    <p>Já tem uma conta? <a href="./dois">Faça o login</a></p>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default Cadastro1
